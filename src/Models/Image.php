b7b6035544280ce54cf33fa83fc7456315916f94
Gracjan Kubicki <gracjan@directinfo.pl>
2018-09-21 00:17:16 +0200

    work progress

--- src/Models/Image.php
+++ src/Models/Image.php
@@ -3,8 +3,14 @@
 namespace Taqie\Image\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image as ImageTransform;
 class Image extends Model
 {
+    const TYPES = [
+        'resize',
+        'fit'
+    ];
     protected $fillable = [
         'imagetable_id',
         'imagetable_type',
@@ -12,4 +18,22 @@ class Image extends Model
         'path',
         'name'
     ];
+
+    protected $appends = [
+        'full_path'
+    ];
+
+    public function getFullPathAttribute()
+    {
+        return Storage::disk($this->getAttribute('disc'))->path($this->getAttribute('path').$this->getAttribute('name'));
+    }
+
+    public function thumbResize(?int $width, ?int $height) : string
+    {
+        $image = ImageTransform::make($this->getAttribute('full_path'));
+        $image->resize($width,$height, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+    }
 }
